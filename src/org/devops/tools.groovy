193c7283b36c8bcb2358da911ea69d16b2b731fe
zhuangdl <42343587+zhuangdl@users.noreply.github.com>
2020-03-27 15:44:21 +0800

    Update tools.groovy

--- src/org/devops/tools.groovy
+++ src/org/devops/tools.groovy
@@ -2,10 +2,9 @@ package org.devops
 
 //格式化输出
 def PrintMes(value,color){
-   colors = [ 'red'     :   "\033[0;31m >>>>>>>>>>>>>${value}<<<<<<<<<<< \033[0m",
-              'blue'    :   "\033[1;28m >>>>>>>>>>>>>${value}<<<<<<<<<<< \033[0m",
+   colors = [ 'red'     :   "\033[1;31m >>>>>>>>>>>>>${value}<<<<<<<<<<< \033[0m",
+              'blue'    :   "\033[1;34m >>>>>>>>>>>>>${value}<<<<<<<<<<< \033[0m",
               'green'   :   "\033[1;32m >>>>>>>>>>>>>${value}<<<<<<<<<<< \033[0m",
-              'green1'  :   "\033[30;32m >>>>>>>>>>>>>${value}<<<<<<<<<<< \033[0m"]
     ansiColor('xterm') {
         println(colors[color])
     }
